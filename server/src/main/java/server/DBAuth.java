06c6d1b038dea421d32056c018f153162d5b6bfd
Renaon <egorbaturin2013@yandex.ru>
2021-04-18 18:55:57 +0300

    Initial commit

---
+++ server/src/main/java/server/DBAuth.java
@@ -0,0 +1,59 @@
+package server;
+
+import java.sql.*;
+
+public class DBAuth implements AuthService{
+    private static Connection connection;
+    private static Statement statement;
+
+    private void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:meowchat.db");
+        statement = connection.createStatement();
+    }
+
+    private void disconnect() {
+        try {
+            statement.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getNicknameByLoginAndPassword(String login, String password) {
+        String sqllogin = "";
+        try{
+            connect();
+            ResultSet res = statement.executeQuery("SELECT LOGIN FROM c_users WHERE LOGIN = " + login +
+                    " AND PASSWORD = " + password + ";");
+            sqllogin = res.getString(1);
+
+            res.close();
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return sqllogin;
+    }
+
+    @Override
+    public boolean registration(String login, String password, String nickname) {
+        int res = 0;
+        try{
+            connect();
+            res = statement.executeUpdate("INSERT INTO c_users (LOGIN, PASSWORD)  VALUES('" +
+                    login + "', '" + password + "');");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return res > 0;
+    }
+}
